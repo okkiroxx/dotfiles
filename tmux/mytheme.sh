12f6d8f3ead10dea2d5f3ef553e8bee1ee97c731
okkiroxx <okkiroxx@gmail.com>
2016-04-11 16:20:21 +0900

    anything

--- tmux/mytheme.sh
+++ tmux/mytheme.sh
@@ -43,9 +43,9 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		"pwd 89 211" \
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
-		"cpu 240 136" \
-		"load 237 167" \
-		#"tmux_mem_cpu_load 237 167" \
+		#"cpu 240 136" \
+		#"load 237 167" \
+		"tmux_mem_cpu_load 237 167" \
 		"battery 137 127" \
 		"weather 37 255" \
 		#"rainbarf 0 0" \
