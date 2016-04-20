d93e24618b78166236dea08d58a776cd88884785
okkiroxx <16040928+okkiroxx@users.noreply.github.com>
2016-04-20 16:46:15 +0900

    modified earthquake.sh

--- tmuxLink.sh
+++ tmuxLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/tmux/.tmux-powerlinerc ~/.tmux-powerlinerc
 ln -sf ~/dotfiles/tmux/mytheme.sh ~/tmux-powerline/themes/mytheme.sh
 ln -sf ~/dotfiles/tmux/mactheme.sh ~/tmux-powerline/themes/mactheme.sh
 ln -sf ~/dotfiles/tmux/cygtheme.sh ~/tmux-powerline/themes/cygtheme.sh
+ln -sf ~/dotfiles/tmux/earthquake.sh ~/tmux-powerline/segments/earthquake.sh
 
