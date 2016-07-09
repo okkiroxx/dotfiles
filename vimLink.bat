d2ed7fa5e20bdfb18868a7e70f9bc7533d711639
okkiroxx <16040928+okkiroxx@users.noreply.github.com>
2016-07-09 17:20:41 +0900

    anything

--- vimLink.bat
+++ vimLink.bat
@@ -1,4 +1,4 @@
 mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
 mklink %HOMEPATH%\_gvimrc %HOMEPATH%\dotfiles\.gvimrc
 mklink /D %HOMEPATH%\.vim\rc %HOMEPATH%\dotfiles\.vim\rc
-exit 0
+mklink /D %HOMEPATH%\vimfiles\syntax %HOMEPATH%\dotfiles\.vim\syntax
