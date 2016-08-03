d94a3c29fb33c987c682bdfd6e7e56900c58f7ee
okkiroxx <16040928+okkiroxx@users.noreply.github.com>
2016-08-03 15:35:17 +0900

    git for windowsテスト

--- tmuxLink.sh
+++ tmuxLink.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/tmux/.tmux-powerlinerc ~/.tmux-powerlinerc
 ln -sf ~/dotfiles/tmux/mytheme.sh ~/tmux-powerline/themes/mytheme.sh
 ln -sf ~/dotfiles/tmux/mactheme.sh ~/tmux-powerline/themes/mactheme.sh
 ln -sf ~/dotfiles/tmux/cygtheme.sh ~/tmux-powerline/themes/cygtheme.sh
+ln -sf ~/dotfiles/tmux/bash_on_windows.sh ~/tmux-powerline/themes/bash_on_windows.sh
 ln -sf ~/dotfiles/tmux/earthquake.sh ~/tmux-powerline/segments/earthquake.sh
 
