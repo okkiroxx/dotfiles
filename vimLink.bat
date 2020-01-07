b2f360301be5b040b11d35191c59479001a665a1
okkiroxx <16040928+okkiroxx@users.noreply.github.com>
2020-01-07 22:00:54 +0900

    anything

--- vimLink.bat
+++ vimLink.bat
@@ -1,7 +1,5 @@
 mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
 mklink %HOMEPATH%\_gvimrc %HOMEPATH%\dotfiles\.gvimrc
-mklink /D %HOMEPATH%\.vim\rc %HOMEPATH%\dotfiles\.vim\rc
-mklink /D %HOMEPATH%\vimfiles\syntax %HOMEPATH%\dotfiles\.vim\syntax
-
-mklink %HOMEPATH%\.vim\evervim.rc.vim %HOMEPATH%\Dropbox\vim\evervim.rc.vim
+mklink /D %HOMEPATH%\.vim %HOMEPATH%\dotfiles\.vim
+mklink /D %HOMEPATH%\vimfiles %HOMEPATH%\dotfiles\.vim
 
