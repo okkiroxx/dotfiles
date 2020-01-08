8fe34f693698d791ecb642948f79d94dec9a45f2
okkiroxx <okkiroxx@gmail.com>
2020-01-08 19:51:00 +0900

    anything

--- tmuxLink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/tmux/.tmux-powerlinerc ~/.tmux-powerlinerc
-ln -sf ~/dotfiles/tmux/mytheme.sh ~/tmux-powerline/themes/mytheme.sh
-ln -sf ~/dotfiles/tmux/mactheme.sh ~/tmux-powerline/themes/mactheme.sh
-ln -sf ~/dotfiles/tmux/cygtheme.sh ~/tmux-powerline/themes/cygtheme.sh
-ln -sf ~/dotfiles/tmux/bash_on_windows.sh ~/tmux-powerline/themes/bash_on_windows.sh
-ln -sf ~/dotfiles/tmux/earthquake.sh ~/tmux-powerline/segments/earthquake.sh
-
