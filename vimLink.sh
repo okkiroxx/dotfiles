395824c90bad4fb850d2d77a5fc3960c0efb0f43
okkiroxx <okkiroxx@gmail.com>
2021-01-14 19:51:53 +0900

    anything

--- vimLink.sh
+++ vimLink.sh
@@ -2,6 +2,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim/rc ~/.vim/rc
-ln -sf ~/dotfiles/.vim/syntax ~/.vim/syntax
-ln -sf ~/dotfiles/.vim/scripts.vim ~/.vim/ftdetect/rtx.vim
 
